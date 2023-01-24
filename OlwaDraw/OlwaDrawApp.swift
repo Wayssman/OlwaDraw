28f8d37abcfc7fb46bf4fda23175e0e9e626c01d
Желанов Александр Валентинович <alexandrzhelanov@gmail.com>
2023-01-24 10:06:40 +0300

    Initial files

---
+++ OlwaDraw/OlwaDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlwaDrawApp.swift
+//  OlwaDraw
+//
+//  Created by Желанов Александр Валентинович on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct OlwaDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
